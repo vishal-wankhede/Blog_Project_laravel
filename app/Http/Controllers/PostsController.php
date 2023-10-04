18b0732117b8e53c9cbb00c3b38de105b4e995fc
vishal.wankhede <vishal.wankhede@droom.in>
2023-10-04 14:26:47 +0530

    Initial commit

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+use Cviebrock\EloquentSluggable\Services\SlugService;
+
+class PostsController extends Controller
+{
+ 
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
+    
+    public function index()
+    {
+        return view('blog.index')
+            ->with('posts', Post::orderBy('updated_at', 'DESC')->get());
+    }
+
+    
+    public function create()
+    {
+        return view('blog.create');
+    }
+
+    
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'image' => 'required|mimes:jpg,png,jpeg|max:5048'
+        ]);
+
+        $newImageName = uniqid() . '-' . $request->title . '.' . $request->image->extension();
+
+        $request->image->move(public_path('images'), $newImageName);
+
+        Post::create([
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            'slug' => SlugService::createSlug(Post::class, 'slug', $request->title),
+            'image_path' => $newImageName,
+            'user_id' => auth()->user()->id
+        ]);
+
+        return redirect('/blog')
+            ->with('message', 'Your post has been added!');
+    }
+
+    
+    public function show($slug)
+    {
+        return view('blog.show')
+            ->with('post', Post::where('slug', $slug)->first());
+    }
+
+    
+    public function edit($slug)
+    {
+        return view('blog.edit')
+            ->with('post', Post::where('slug', $slug)->first());
+    }
+
+    
+    public function update(Request $request, $slug)
+    {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+
+        Post::where('slug', $slug)
+            ->update([
+                'title' => $request->input('title'),
+                'description' => $request->input('description'),
+                'slug' => SlugService::createSlug(Post::class, 'slug', $request->title),
+                'user_id' => auth()->user()->id
+            ]);
+
+        return redirect('/blog')
+            ->with('message', 'Your post has been updated!');
+    }
+
+    
+    public function destroy($slug)
+    {
+        $post = Post::where('slug', $slug);
+        $post->delete();
+
+        return redirect('/blog')
+            ->with('message', 'Your post has been deleted!');
+    }
+}
+
